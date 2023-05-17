f55ce1d15ff5aa108ed52ef31a2ba25cfb3447c5
Buy-One <61803004+Buy-One@users.noreply.github.com>
2023-05-17 12:32:48 +0300

    Update BuyOne_Scroll horizontally with variable speed.lua

--- Misc/BuyOne_Variable speed scrolling/BuyOne_Scroll horizontally with variable speed.lua
+++ Misc/BuyOne_Variable speed scrolling/BuyOne_Scroll horizontally with variable speed.lua
@@ -2,21 +2,19 @@
 ReaScript name: BuyOne_Scroll horizontally with variable speed.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.0
-Changelog: #Initial release
+Version: 1.1
+Changelog: 
+	 1.1 #Added support for fractional beats
 Licence: WTFPL
 REAPER: at least v5.962
 Extensions: SWS/S&M or js_ReaScriptAPI recommended
-About: Alternative to the native 'View: Scroll horizontally (MIDI CC relative/mousewheel)' 
-       action which scrolls by exactly 122 px regardless of the zoom level.
-
-       The script's scroll unit is 16 px per execution which 
-       can be increased with the SPEED setting in the USER SETTINGS.
-
-       Bind to mousewheel (optionally with modifiers).  
-
-       With vertical mousewheel the default direction is up - right, down - left,
-       to reverse the direction enable MW_REVERSE setting in the USER SETTINGS
+About: 	Alternative to the native 'View: Scroll horizontally (MIDI CC relative/mousewheel)' 
+	action which scrolls by exactly 122 px regardless of the zoom level.
+	   
+	Bind to mousewheel (optionally with modifiers).  
+	   
+	With vertical mousewheel the default direction is up - right, down - left,
+	to reverse the direction enable MW_REVERSE setting in the USER SETTINGS
 
 ]]
 
@@ -24,17 +22,25 @@ About: Alternative to the native 'View: Scroll horizontally (MIDI CC relative/mo
 ------------------------------ USER SETTINGS --------------------------------
 -----------------------------------------------------------------------------
 
--- Insert integer (whole number) between the quotes;
--- empty or invalid defaults to 16 px per one script execution,
+-- Insert a number between the quotes,
+-- when BY_BEATS setting is enabled below, both integers (whole numbers)
+-- and decimal numbers are supported, numbers less than 1 will shorten
+-- 1 beat: 0.5 to half (= 1/8), 0.75 to 3/4th (= 1/8 dotted), 
+-- 0.25 to 1/4th (= 1/16) etc., non-musical divisions and values 
+-- in the format of '1/2' are supported as well;
+-- when BY_BEATS setting isn't enabled only integers are supported;
+-- empty or invalid defaults to either 16 px or 1 beat (if BY_BEATS
+-- setting is enabled) per single mousewheel nudge,
 -- otherwise the value is used as a factor
--- by which the default value (16 px) is multiplied
+-- by which the default value (16 px or 1 beat) is multiplied
 SPEED = ""
 
 -- Enable by placing any alphanumeric character between the quotes,
 -- only relevant for SPEED setting above,
 -- if enabled, the numeric value of the SPEED setting becomes
 -- the number of beats to scroll by rather than a factor
--- to multiply pixels by, empty SPEED setting in this case equals 1;
+-- to multiply pixels by, empty or invalid SPEED setting 
+-- in this case equals 1;
 -- doesn't have any effect if PAGING_SCROLL setting is enabled below;
 -- at higher zoom-in levels scroll step (beat * SPEED) is reduced
 -- to ensure slower scrolling for ease of following the time line
@@ -87,8 +93,11 @@ local is_new_value,filename,sectionID,cmdID,mode,resolution,val = r.get_action_c
 end
 
 
-SPEED = (not tonumber(SPEED) or tonumber(SPEED) and SPEED+0 == 0) and 1 or math.floor(math.abs(tonumber(SPEED))) -- ignoring non-numerals, zero, any decimal and negative values
 BY_BEATS = #BY_BEATS:gsub(' ','') > 0
+local a,b = SPEED:match('(%d+)/(%d+)')
+SPEED = BY_BEATS and a and b and a/b or SPEED
+SPEED = (not tonumber(SPEED) or tonumber(SPEED) and SPEED+0 == 0 or not BY_BEATS and SPEED:match('%.')) and 1 or BY_BEATS and math.abs(SPEED) or math.floor(math.abs(SPEED)) -- ignoring non-numerals, zero, decimals when beats aren't enabled and negative values // negative numerals math.floor rounds down to a greater natural number, -1.1 is rounded down to -2
+
 PAGING_SCROLL = #PAGING_SCROLL:gsub(' ','') > 0
 MW_REVERSE = #MW_REVERSE:gsub(' ','') > 0
 
@@ -110,7 +119,6 @@ MW_REVERSE = #MW_REVERSE:gsub(' ','') > 0
 local dir = Mouse_Wheel_Direction(val, MW_REVERSE)
 
 r.CSurf_OnScroll(SPEED*dir, 0)
-
 
 
 
