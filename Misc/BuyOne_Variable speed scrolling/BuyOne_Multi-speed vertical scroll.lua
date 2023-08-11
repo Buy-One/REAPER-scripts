7bc6666b1069e636aa0885918a538790a0a4d2a1
Buy-One <61803004+Buy-One@users.noreply.github.com>
2023-08-11 21:16:38 +0300

    Update BuyOne_Multi-speed vertical scroll.lua

--- Misc/BuyOne_Variable speed scrolling/BuyOne_Multi-speed vertical scroll.lua
+++ Misc/BuyOne_Variable speed scrolling/BuyOne_Multi-speed vertical scroll.lua
@@ -2,11 +2,12 @@
 ReaScript name: BuyOne_Multi-speed vertical scroll.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.4
-Changelog: v1.4 #Updated info text in the USER SETTINGS
+Version: 1.5
+Changelog: v1.5 #Fixed REAPER version evaluation
+	   v1.4 #Updated info text in the USER SETTINGS
 	   v1.3 #Updated info text in the USER SETTINGS
 	   v1.2 #Fixed scroll when the mouse is over the TCP displayed on the right side of the Arrange
-		view at certain horizontal scroll position
+	   	view at certain horizontal scroll position
 	   v1.1 #Removed limitation on using Ctrl+Shift modifier when extensions aren't installed
 Licence: WTFPL
 REAPER: at least v5.962
@@ -568,7 +569,7 @@ local dock_change = Detect_Docker_Pane_Change(wnd_ident_t, 2) or dock_change
 	
 	-- get 'Maximum vertical zoom' set at Preferences -> Editing behavior, which affects max track height set with 'View: Toggle track zoom to maximum height', introduced in build 6.76
 	local cont
-		if tonumber(r.GetAppVersion():match('(.+)/?')) >= 6.76 then
+		if tonumber(r.GetAppVersion():match('[%d%.]+')) >= 6.76 then
 		local f = io.open(r.get_ini_file(),'r')
 		cont = f:read('*a')
 		f:close()
