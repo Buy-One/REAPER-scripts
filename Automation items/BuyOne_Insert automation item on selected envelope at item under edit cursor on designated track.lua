7cb6edb716b255c77329888a6c94d8c16f9a17f6
Buy-One <61803004+Buy-One@users.noreply.github.com>
2023-12-23 22:55:33 +0200

    Update BuyOne_Insert automation item on selected envelope at item under edit cursor on designated track.lua

--- Automation items/BuyOne_Insert automation item on selected envelope at item under edit cursor on designated track.lua
+++ Automation items/BuyOne_Insert automation item on selected envelope at item under edit cursor on designated track.lua
@@ -153,7 +153,7 @@ local env_tr = r.GetEnvelopeInfo_Value(env, 'P_TRACK')
 
 -- get index of the first selected AI on the envelope, which will be pool source for the newly inserted AI
 local GetSetAI = r.GetSetAutomationItemInfo
-local pool_src_idx, pre_existing_AI
+local pool_src_idx
 	for AI_idx = 0, r.CountAutomationItems(env)-1 do
 		if GetSetAI(env, AI_idx, 'D_UISEL', -1, false) > 0 -- selected; value -1, is_set false
 		then
