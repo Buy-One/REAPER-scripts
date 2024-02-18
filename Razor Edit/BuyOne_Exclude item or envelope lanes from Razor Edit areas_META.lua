dba2e8b9920979567ba551410e762359926c1831
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-02-18 20:34:21 +0200

    Update BuyOne_Exclude item or envelope lanes from Razor Edit areas_META.lua

--- Razor Edit/BuyOne_Exclude item or envelope lanes from Razor Edit areas_META.lua
+++ Razor Edit/BuyOne_Exclude item or envelope lanes from Razor Edit areas_META.lua
@@ -1,46 +1,49 @@
 --[[
 ReaScript name: BuyOne_Exclude item or envelope lanes from Razor Edit areas_META.lua (4 scripts)
 Author: BuyOne
-Version: 1.1
-Changelog: v1.1 #Added REAPER version check
+Version: 1.2
+Changelog:  v1.2 #Creation of individual scripts has been made hands-free. 
+		 These are created in the directory the META script is located in
+		 and from there are imported into the Action list.
+		 #Updated About text
+	   v1.1 #Added REAPER version check
 		#Corrected minimal supported version in the header
 Author URL: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
 Licence: WTFPL
 REAPER: at least v6.24
 Metapackage: true
-Provides:   [main] . > BuyOne_Exclude item lanes from Razor Edit areas on all tracks.lua
-            [main] . > BuyOne_Exclude item lanes from Razor Edit areas on selected tracks.lua
-            [main] . > BuyOne_Exclude envelope lanes from Razor Edit areas on all tracks.lua
-            [main] . > BuyOne_Exclude envelope lanes from Razor Edit areas on selected tracks.lua
-About:	If this script name is suffixed with META it will spawn 
-    		all individual scripts included in the package into 
-    		the directory supplied by the user in a dialogue.
-    		These can then be manually imported into the Action list 
-    		from any other location. If there's no META suffix 
-    		in this script name it will perfom the operation indicated 
-    		in its name.
-    
-    		The target lane is only excluded from a Razor Edit area
-    		if the area includes both item and envelope lanes, otherwise
-    		Razor Edit area remains intact. 
-    		When envelope is displayed in the item lane due to option
-    		'Move to media liane' being enabled, the envelope isn't excluded
-    		from a Razor Edit area because in this case envelope lane doesn't
-    		exist and usual Razor Edit operations cannot be performed on such
-    		envelope.
-    		
-    		The scripts:
-    		Exclude item lanes from Razor Edit areas on all tracks.lua
-    		Exclude item lanes from Razor Edit areas on selected tracks.lua
-    		
-    		Can be used inside a custom action alongside the native action
-    		'Razor edit: Create area from cursor to mouse'
-    		to only create Razor Edit area on envelope lanes because the native
-    		action doesn't allow isolating them, e.g.
-    		
-    		Custom: Create Razor edit area from cursor to mouse on envelope lane
-    			Razor edit: Create area from cursor to mouse
-    			Script: BuyOne_Exclude item lanes from Razor Edit areas on selected tracks.lua
+Provides: 	[main] . > BuyOne_Exclude item lanes from Razor Edit areas on all tracks.lua
+		[main] . > BuyOne_Exclude item lanes from Razor Edit areas on selected tracks.lua
+		[main] . > BuyOne_Exclude envelope lanes from Razor Edit areas on all tracks.lua
+		[main] . > BuyOne_Exclude envelope lanes from Razor Edit areas on selected tracks.lua
+About:	If this script name is suffixed with META, when executed it 
+	will automatically spawn all individual scripts included in 
+	the package into the directory of the META script and will 
+	import them into the Action list from that directory.  
+	If there's no META suffix in this script name it will perfom 
+	the operation indicated in its name.
+
+	The target lane is only excluded from a Razor Edit area
+	if the area includes both item and envelope lanes, otherwise
+	Razor Edit area remains intact. 
+	When envelope is displayed in the item lane due to option
+	'Move to media liane' being enabled, the envelope isn't excluded
+	from a Razor Edit area because in this case envelope lane doesn't
+	exist and usual Razor Edit operations cannot be performed on such
+	envelope.
+	
+	The scripts:
+	Exclude item lanes from Razor Edit areas on all tracks.lua
+	Exclude item lanes from Razor Edit areas on selected tracks.lua
+	
+	Can be used inside a custom action alongside the native action
+	'Razor edit: Create area from cursor to mouse'
+	to only create Razor Edit area on envelope lanes because the native
+	action doesn't allow isolating them, e.g.
+	
+	Custom: Create Razor edit area from cursor to mouse on envelope lane
+		Razor edit: Create area from cursor to mouse
+		Script: BuyOne_Exclude item lanes from Razor Edit areas on selected tracks.lua
 
 ]]
 
@@ -72,6 +75,22 @@ function META_Spawn_Scripts(fullpath, scr_name, names_t)
 	return mess:match('Permission denied') and path..sep -- dir exists // this one is enough
 	end
 
+	local function Esc(str)
+		if not str then return end -- prevents error
+	-- isolating the 1st return value so that if vars are initialized in a row outside of the function the next var isn't assigned the 2nd return value
+	local str = str:gsub('[%(%)%+%-%[%]%.%^%$%*%?%%]','%%%0')
+	return str
+	end
+	
+	local function script_is_installed(fullpath)
+	local sep = r.GetResourcePath():match('[\\/]')
+		for line in io.lines(r.GetResourcePath()..sep..'reaper-kb.ini') do
+		local path = line and line:match('.-%.lua["%s]*(.-)"?')
+			if path and fullpath:match(Esc(path)) then -- installed 
+			return true end
+		end
+	end
+
 	if not fullpath:match(Esc(scr_name)) then return true end -- will allow to continue the script execution outside, since it's not a META script
 
 local names_t, content = names_t
@@ -83,9 +102,9 @@ local names_t, content = names_t
 	this_script:close()
 	names_t, found = {}
 		for line in content:gmatch('[^\n\r]+') do
-			if line and line:match('Provides') then found = 1 end
+			if line and line:match('Provides:') then found = 1 end
 			if found and line:match('%.lua') then
-			names_t[#names_t+1] = line:match('.+[/](.+[%w])') or line:match('BuyOne.+[%w]') -- in case the new script name line includes a subfolder path, the subfolder won't be created, trimming trailing spaces if any because they invalidate file path
+			names_t[#names_t+1] = line:match('.+[/](.+)') or line:match('BuyOne.+[%w]') -- in case the new script name line includes a subfolder path, the subfolder won't be created
 			elseif found and #names_t > 0 then
 			break -- the list has ended
 			end
@@ -93,25 +112,28 @@ local names_t, content = names_t
 	end
 
 	if names_t and #names_t > 0 then
+	
+--[[ GETTING PATH FROM THE USER INPUT
 
 	r.MB('              This meta script will spawn '..#names_t
 	..'\n\n     individual scripts included in the package'
 	..'\n\n     after you supply a path to the directory\n\n\t    they will be placed in'
 	..'\n\n\twhich can be temporary.\n\n           After that the spawned scripts'
 	..'\n\n will have to be imported into the Action list.','META',0)
-
+	
 	local ret, output -- to be able to autofill the dialogue with last entry on RELOAD
-
+	
 	::RETRY::
 	ret, output = r.GetUserInputs('Scripts destination folder', 1,
 	'Full path to the dest. folder, extrawidth=200', output or '')
 
 		if not ret or #output:gsub(' ','') == 0 then return end -- must be aborted outside of the function
 
-	local user_path = Dir_Exists(output) -- validate user supplied path
-		if not user_path then Error_Tooltip('\n\n invalid path \n\n', 1, 1) -- caps, spaced true
+	local path = Dir_Exists(output) -- validate user supplied path
+		if not path then Error_Tooltip('\n\n invalid path \n\n', 1, 1) -- caps, spaced true
 		goto RETRY end
-
+	]]	
+	
 		-- load this script if wasn't loaded above to parse the header for file names list
 		if not content then
 		local this_script = io.open(fullpath, 'r')
@@ -119,13 +141,25 @@ local names_t, content = names_t
 		this_script:close()
 		end
 
+		local path = fullpath:match('(.+[\\/])') -- WHEN NOT GETTING PATH FROM USER INPUT, USE META SCRIPT PATH
+		
 		-- spawn scripts
 		for k, scr_name in ipairs(names_t) do
-		local new_script = io.open(user_path..scr_name, 'w') -- create new file
+		local new_script = io.open(path..scr_name, 'w') -- create new file
 		content = content:gsub('ReaScript name:.-\n', 'ReaScript name: '..scr_name..'\n', 1) -- replace script name in the About tag
 		new_script:write(content)
 		new_script:close()
 		end
+		
+		-- CONDITION BY THE SCRIPT BEING INSTALLED TO OTHERWISE ALLOW SPAWNING SCRIPTS WITH BATCH SCRIPT INSTALLER VIA dofile() WITHOUT INSTALLATION ONLY FOR THE SAKE OF SETTNIGS TRANSFER, get_action_context() is useless as a conditon since when this script is executed via dofile() from the installer script the function returns props of the latter		
+		if script_is_installed(fullpath) then
+			for _, sectID in ipairs{0} do -- Main // per script list
+				for k, scr_name in ipairs(names_t) do
+				local result = r.AddRemoveReaScript(true, sectID, path..scr_name, true) -- add, commit true // doesn't affect the props of an already installed script if attempts to install it again, so is safe
+				end
+			end
+		end
+		
 	end
 
 end
@@ -249,8 +283,8 @@ end
 	Error_Tooltip('\n\n\tthe script is supported \n\n in REAPER builds 6.24 onwards \n\n', 1, 1) -- caps, spaced true
 	return r.defer(no_undo) end
 
-
 local is_new_value, fullpath, sectionID, cmdID, mode, resolution, val = r.get_action_context()
+fullpath = debug.getinfo(1,'S').source:match('^@?(.+)') -- if the script is run via dofile() from installer script the above function will return installer script path which is irrelevant for this script
 local scr_name = fullpath:match('.+[\\/].-_(.+)%.%w+') -- without path, extension and author name
 
 	-- doesn't run in non-META scripts
@@ -258,7 +292,6 @@ local scr_name = fullpath:match('.+[\\/].-_(.+)%.%w+') -- without path, extensio
 	then return r.defer(no_undo) end -- abort if META script but continue if not
 
 --scr_name = 'Exclude item lanes from Razor Edit areas on all tracks' ------------------- NAME TEST
-
 
 local not_elm1 = Invalid_Script_Name(scr_name, 'item', 'envelope')
 local not_elm2 = Invalid_Script_Name(scr_name, 'selected tracks', 'all tracks')
