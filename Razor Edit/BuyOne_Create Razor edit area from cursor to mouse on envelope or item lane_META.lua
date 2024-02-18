4b7190bce2a3312694c7c16a93da3c9783293645
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-02-18 20:29:21 +0200

    Update BuyOne_Create Razor edit area from cursor to mouse on envelope or item lane_META.lua

--- Razor Edit/BuyOne_Create Razor edit area from cursor to mouse on envelope or item lane_META.lua
+++ Razor Edit/BuyOne_Create Razor edit area from cursor to mouse on envelope or item lane_META.lua
@@ -3,7 +3,11 @@ ReaScript name: BuyOne_Create Razor edit area from cursor to mouse on envelope o
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
 Version: 1.1
-Changelog: v1.1 #Fixed error message due to duplicate statement
+Changelog:  v1.1 #Fixed error message due to duplicate statement
+		 #Creation of individual scripts has been made hands-free. 
+		 These are created in the directory the META script is located in
+		 and from there are imported into the Action list.
+		 #Updated About text
 Metapackage: true
 Licence: WTFPL
 REAPER: at least v6.24
@@ -16,56 +20,55 @@ Provides:	. > BuyOne_Create Razor edit area from cursor to mouse on any lane.lua
 		. > BuyOne_Create Razor edit area from cursor to mouse on track all envelope lanes keeping existing.lua
 		. > BuyOne_Create Razor edit area from cursor to mouse on item lane.lua
 		. > BuyOne_Create Razor edit area from cursor to mouse on item lane keeping existing.lua
-About:	If this script name is suffixed with META it will spawn 
-    		all individual scripts included in the package into 
-    		the directory supplied by the user in a dialogue.
-    		These can then be manually imported into the Action list 
-    		from any other location. If there's no META suffix 
-    		in this script name it will perfom the operation indicated 
-    		in its name.
-    
-    		The scripts refine the functionality of the native action 
-    		'Razor edit: Create area from cursor to mouse'
-    		which can create Razor edit area either on item lane only 
-    		or on item lane and all envelope lanes but cannot
-    		create Razor edit area on a single envelope lane, and it works 
-    		exclusively, removing any existing Razor edit areas on the track
-    		when the new one is being created.		
-    
-    		The scripts must be run with a shortcut so that the mouse 
-    		cursor is free to point at a location on the timeline.
-    		
-    		If SWS/S&M extension isn't installed, the target envelope 
-    		must be selected, otherwise it suffices that the mouse cursor 
-    		point at it.
-    		If the extension is installed and the mouse doesn't point at 
-    		an envelope, a Razor edit area will be created on envelope lanes
-    		as long as there's selected envelope. 
-    		EXCEPTIONS
-    		1) Without the SWS extension being installed, in order to target 
-    		an envelope lane with the following scripts:
-    		Create Razor edit area from cursor to mouse on any lane.lua
-    		AND
-    		Create Razor edit area from cursor to mouse on any lane keeping existing.lua
-    		such envelope lane must be both selected and under the mouse.
-    		2) The following scripts don't require pointing mouse at the envelope
-    		lane (if the SWS is installed) or having at least one envelope selected
-    		(if the extension isn't installed), the mouse can point at an item
-    		lane as well:
-    		Create Razor edit area from cursor to mouse on track all envelope lanes.lua
-    		AND
-    		Create Razor edit area from cursor to mouse on track all envelope lanes keeping existing.lua
-    				
-    		In scripts which include 'keeping existing' verbiage in their name
-    		the existing Razor edit areas on the target lane (item or envelope or
-    		both, depending on the script designation) are retained, while
-    		Razor edit areas which fall between the edit and mouse cursor on the 
-    		target lane(s) get overwritten.
-    		
-    		When item lane is the target, if mouse cursor is over the TCP 
-    		the Razor edit area will be created up to the TCP. When envelope lane 
-    		is the target, if mouse cursor is over the ECP (envelope control panel) 
-    		the Razor edit area will be created up to the ECP.
+About:	If this script name is suffixed with META, when executed it 
+	will automatically spawn all individual scripts included in 
+	the package into the directory of the META script and will 
+	import them into the Action list from that directory.  
+	If there's no META suffix in this script name it will perfom 
+	the operation indicated in its name.
+
+	The scripts refine the functionality of the native action 
+	'Razor edit: Create area from cursor to mouse'
+	which can create Razor edit area either on item lane only 
+	or on item lane and all envelope lanes but cannot
+	create Razor edit area on a single envelope lane, and it works 
+	exclusively, removing any existing Razor edit areas on the track
+	when the new one is being created.		
+
+	The scripts must be run with a shortcut so that the mouse 
+	cursor is free to point at a location on the timeline.
+	
+	If SWS/S&M extension isn't installed, the target envelope 
+	must be selected, otherwise it suffices that the mouse cursor 
+	point at it.
+	If the extension is installed and the mouse doesn't point at 
+	an envelope, a Razor edit area will be created on envelope lanes
+	as long as there's selected envelope. 
+	EXCEPTIONS
+	1) Without the SWS extension being installed, in order to target 
+	an envelope lane with the following scripts:
+	Create Razor edit area from cursor to mouse on any lane.lua
+	AND
+	Create Razor edit area from cursor to mouse on any lane keeping existing.lua
+	such envelope lane must be both selected and under the mouse.
+	2) The following scripts don't require pointing mouse at the envelope
+	lane (if the SWS is installed) or having at least one envelope selected
+	(if the extension isn't installed), the mouse can point at an item
+	lane as well:
+	Create Razor edit area from cursor to mouse on track all envelope lanes.lua
+	AND
+	Create Razor edit area from cursor to mouse on track all envelope lanes keeping existing.lua
+			
+	In scripts which include 'keeping existing' verbiage in their name
+	the existing Razor edit areas on the target lane (item or envelope or
+	both, depending on the script designation) are retained, while
+	Razor edit areas which fall between the edit and mouse cursor on the 
+	target lane(s) get overwritten.
+	
+	When item lane is the target, if mouse cursor is over the TCP 
+	the Razor edit area will be created up to the TCP. When envelope lane 
+	is the target, if mouse cursor is over the ECP (envelope control panel) 
+	the Razor edit area will be created up to the ECP.
 		
 ]]
 
@@ -105,8 +108,17 @@ function META_Spawn_Scripts(fullpath, scr_name, names_t)
 	local str = str:gsub('[%(%)%+%-%[%]%.%^%$%*%?%%]','%%%0')
 	return str
 	end
+	
+	local function script_is_installed(fullpath)
+	local sep = r.GetResourcePath():match('[\\/]')
+		for line in io.lines(r.GetResourcePath()..sep..'reaper-kb.ini') do
+		local path = line and line:match('.-%.lua["%s]*(.-)"?')
+			if path and fullpath:match(Esc(path)) then -- installed 
+			return true end
+		end
+	end
 
-		if not fullpath:match(Esc(scr_name)) then return true end -- will allow to continue the script execution outside, since it's not a META script
+	if not fullpath:match(Esc(scr_name)) then return true end -- will allow to continue the script execution outside, since it's not a META script
 
 local names_t, content = names_t
 
@@ -117,9 +129,9 @@ local names_t, content = names_t
 	this_script:close()
 	names_t, found = {}
 		for line in content:gmatch('[^\n\r]+') do
-			if line and line:match('Provides') then found = 1 end
+			if line and line:match('Provides:') then found = 1 end
 			if found and line:match('%.lua') then
-			names_t[#names_t+1] = line:match('.+[/](.+[%w])') or line:match('BuyOne.+[%w]') -- in case the new script name line includes a subfolder path, the subfolder won't be created, trimming trailing spaces if any because they invalidate file path
+			names_t[#names_t+1] = line:match('.+[/](.+)') or line:match('BuyOne.+[%w]') -- in case the new script name line includes a subfolder path, the subfolder won't be created
 			elseif found and #names_t > 0 then
 			break -- the list has ended
 			end
@@ -127,25 +139,28 @@ local names_t, content = names_t
 	end
 
 	if names_t and #names_t > 0 then
+	
+--[[ GETTING PATH FROM THE USER INPUT
 
 	r.MB('              This meta script will spawn '..#names_t
 	..'\n\n     individual scripts included in the package'
 	..'\n\n     after you supply a path to the directory\n\n\t    they will be placed in'
 	..'\n\n\twhich can be temporary.\n\n           After that the spawned scripts'
 	..'\n\n will have to be imported into the Action list.','META',0)
-
+	
 	local ret, output -- to be able to autofill the dialogue with last entry on RELOAD
-
+	
 	::RETRY::
 	ret, output = r.GetUserInputs('Scripts destination folder', 1,
 	'Full path to the dest. folder, extrawidth=200', output or '')
 
 		if not ret or #output:gsub(' ','') == 0 then return end -- must be aborted outside of the function
 
-	local user_path = Dir_Exists(output) -- validate user supplied path
-		if not user_path then Error_Tooltip('\n\n invalid path \n\n', 1, 1) -- caps, spaced true
+	local path = Dir_Exists(output) -- validate user supplied path
+		if not path then Error_Tooltip('\n\n invalid path \n\n', 1, 1) -- caps, spaced true
 		goto RETRY end
-
+	]]	
+	
 		-- load this script if wasn't loaded above to parse the header for file names list
 		if not content then
 		local this_script = io.open(fullpath, 'r')
@@ -153,13 +168,25 @@ local names_t, content = names_t
 		this_script:close()
 		end
 
+		local path = fullpath:match('(.+[\\/])') -- WHEN NOT GETTING PATH FROM USER INPUT, USE META SCRIPT PATH
+		
 		-- spawn scripts
 		for k, scr_name in ipairs(names_t) do
-		local new_script = io.open(user_path..scr_name, 'w') -- create new file
+		local new_script = io.open(path..scr_name, 'w') -- create new file
 		content = content:gsub('ReaScript name:.-\n', 'ReaScript name: '..scr_name..'\n', 1) -- replace script name in the About tag
 		new_script:write(content)
 		new_script:close()
 		end
+		
+		-- CONDITION BY THE SCRIPT BEING INSTALLED TO OTHERWISE ALLOW SPAWNING SCRIPTS WITH BATCH SCRIPT INSTALLER VIA dofile() WITHOUT INSTALLATION ONLY FOR THE SAKE OF SETTNIGS TRANSFER, get_action_context() is useless as a conditon since when this script is executed via dofile() from the installer script the function returns props of the latter		
+		if script_is_installed(fullpath) then
+			for _, sectID in ipairs{0} do -- Main // per script list
+				for k, scr_name in ipairs(names_t) do
+				local result = r.AddRemoveReaScript(true, sectID, path..scr_name, true) -- add, commit true // doesn't affect the props of an already installed script if attempts to install it again, so is safe
+				end
+			end
+		end
+		
 	end
 
 end
@@ -287,7 +314,8 @@ end
 	return r.defer(no_undo) end
 
 local is_new_value, fullpath, sectionID, cmdID, mode, resolution, val = r.get_action_context()
-local scr_name = fullpath:match('[^\\/_]+') -- without path, author's name
+fullpath = debug.getinfo(1,'S').source:match('^@?(.+)') -- if the script is run via dofile() from installer script the above function will return installer script path which is irrelevant for this script
+local scr_name = fullpath:match('[^\\/_]+') -- without path, author's namee
 
 
 	-- doesn't run in non-META scripts
@@ -393,7 +421,7 @@ local data = ''
 
 	elseif item_lane then
 	-- Create Razor edit area from cursor to mouse on item lane
-	-- Create Razor edit area from cursor to mouse on item lane keeping existing
+	-- Create Razor edit area from cursor to mouse on item lane keeping existing		
 		if keeping then
 		data = Exclude_Raz_Edit_Areas(raz_edit_data, st, fin, env_lane, item_lane)
 		end
