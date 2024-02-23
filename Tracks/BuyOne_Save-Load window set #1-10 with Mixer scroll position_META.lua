2922b640836b91a9a17cfec85e1e0a331a1d1507
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-02-23 21:46:45 +0200

    Update BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua

--- Tracks/BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua
+++ Tracks/BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua
@@ -2,15 +2,17 @@
 ReaScript name: BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua (10 scripts)
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.5
-Changelog:  v1.5 #Creation of individual scripts has been made hands-free. 
+Version: 1.6
+Changelog:  v1.6 #Fixed individual script installation function
+		 #Made individual script installation function more effici
+	    v1.5 #Creation of individual scripts has been made hands-free. 
 		 These are created in the directory the META script is located in
 		 and from there are imported into the Action list.
 		 #Updated About text
 	    v1.4 #Added functionality to export individual scripts included in the package
 		 #Updated About text
-	    v1.3 #Fixed error prone pieces of code
-	    v1.2 #Fixed capture of MCP folder state
+	   v1.3 #Fixed error prone pieces of code
+	   v1.2 #Fixed capture of MCP folder state
 		 #Fixed logic of the search for the parent of a collapsed folder the target track is in
 	    v1.1 #More reliable scroll in different situations
 Licence: WTFPL
@@ -151,7 +153,7 @@ About:	If this script name is suffixed with META, when executed it will
 	Check out also BuyOne_Scroll named track into view in the Mixer (guide inside).lua
 	for a method of scrolling named track into view without hard linking between
 	track name and screenset number.
-		
+	
 ]]
 
 
@@ -168,7 +170,7 @@ do return end
 end
 
 
-function META_Spawn_Scripts(fullpath, scr_name, names_t)
+function META_Spawn_Scripts(fullpath, fullpath_init, scr_name, names_t)
 
 	local function Dir_Exists(path) -- short
 	local path = path:match('^%s*(.-)%s*$') -- remove leading/trailing spaces
@@ -189,7 +191,7 @@ function META_Spawn_Scripts(fullpath, scr_name, names_t)
 	local sep = r.GetResourcePath():match('[\\/]')
 		for line in io.lines(r.GetResourcePath()..sep..'reaper-kb.ini') do
 		local path = line and line:match('.-%.lua["%s]*(.-)"?')
-			if path and fullpath:match(Esc(path)) then -- installed 
+			if path and #path > 0 and fullpath:match(Esc(path)) then -- installed 
 			return true end
 		end
 	end
@@ -254,8 +256,10 @@ local names_t, content = names_t
 		new_script:close()
 		end
 		
-		-- CONDITION BY THE SCRIPT BEING INSTALLED TO OTHERWISE ALLOW SPAWNING SCRIPTS WITH BATCH SCRIPT INSTALLER VIA dofile() WITHOUT INSTALLATION ONLY FOR THE SAKE OF SETTNIGS TRANSFER, get_action_context() is useless as a conditon since when this script is executed via dofile() from the installer script the function returns props of the latter		
-		if script_is_installed(fullpath) then
+		-- CONDITION BY THE SCRIPT BEING INSTALLED TO OTHERWISE ALLOW SPAWNING SCRIPTS WITH INSTALLER SCRIPT VIA dofile() WITHOUT INSTALLATION ONLY FOR THE SAKE OF SETTINGS TRANSFER WHICH IS SUPPOSED TO BE DONE WHILE THE SCRIPT IS IN A TEMP FOLDER, get_action_context() alone is useless as a condition since when this script is executed via dofile() from the installer script the function returns props of the latter
+	--	if script_is_installed(fullpath) then -- install individual scripts
+	-- OR, which is more efficient, in the scenario described above this condition will be false
+		if fullpath_init:match('.+[\\/](.+)') == scr_name then -- install individual scripts
 			for _, sectID in ipairs{0} do -- Main // per script list
 				for k, scr_name in ipairs(names_t) do
 				local result = r.AddRemoveReaScript(true, sectID, path..scr_name, true) -- add, commit true // doesn't affect the props of an already installed script if attempts to install it again, so is safe
@@ -543,11 +547,11 @@ end
 
 local id = 404 -- 1st three digits of save/load screenset actions command IDs
 local context = {r.get_action_context()}
-context[2] = debug.getinfo(1,'S').source:match('^@?(.+)') -- if the script is run via dofile() from installer script the above function will return installer script path which is irrelevant for this script
-local scr_name = context[2]:match('[^\\/]-_(.+)%.lua') -- without path, scripter name & ext
+local fullpath = debug.getinfo(1,'S').source:match('^@?(.+)') -- if the script is run via dofile() from installer script the above function will return installer script path which is irrelevant for this script
+local scr_name = fullpath:match('[^\\/]-_(.+)%.lua') -- without path, scripter name & extxt
 
 	-- doesn't run in non-META scripts
-	if not META_Spawn_Scripts(context[2], 'BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua', names_t)
+	if not META_Spawn_Scripts(fullpath, context[2], 'BuyOne_Save-Load window set #1-10 with Mixer scroll position_META.lua', names_t)
 	then return r.defer(no_undo) end -- abort if META script but continue if not
 
 local set_num = tonumber(context[2]:match('[^\\/_].+#(%d+)')) -- get set # from script name
