51edebecd77849197d3e22810bcda033f90fe0e8
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-03-03 09:37:35 +0200

    Update BuyOne_Insert new track automatically sending it to track specified in the settings.lua

--- Tracks/BuyOne_Insert new track automatically sending it to track specified in the settings.lua
+++ Tracks/BuyOne_Insert new track automatically sending it to track specified in the settings.lua
@@ -2,8 +2,8 @@
 ReaScript name: BuyOne_Insert new track automatically sending it to track specified in the settings.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.0
-Changelog: #Initial release
+Version: 1.1
+Changelog: v1.1 #Additional SEND_MODE error proofing
 Licence: WTFPL
 REAPER: at least v5.962
 Extensions: 
@@ -20,7 +20,7 @@ DESTINATION_TRACK_NAME = ""
 
 -- Between the quotes insert the number corresponding to the send mode:
 -- 1 - post-fader (post-pan), 2 - pre-fader (pre-fx), 3 - pre-fader (post-fx)
--- if not set defaults to 1
+-- if not set or invalid, defaults to 1
 SEND_MODE = ""
 
 
@@ -49,7 +49,7 @@ end
 
 local x, y = r.GetMousePosition()
 
-DESTINATION_TRACK_NAME = DESTINATION_TRACK_NAME:match('^%s*(.-)%s*$')--('[%w%p].*[%w%p]*')
+DESTINATION_TRACK_NAME = DESTINATION_TRACK_NAME:match('^%s*(.-)%s*$') -- stripping leading & trailing spaces
 
 
 	if #DESTINATION_TRACK_NAME == 0 then
@@ -63,7 +63,8 @@ local bus_tr
 	for i=0,r.GetNumTracks()-1 do --find the bus track
 	local tr = r.GetTrack(0,i)
 	local retval, name = r.GetTrackName(tr)
-		if name:match('^%s*'..Esc(DESTINATION_TRACK_NAME)..'%s*$') then bus_tr = tr break end
+		if name:match('^%s*'..Esc(DESTINATION_TRACK_NAME)..'%s*$') -- stripping leading & trailing spaces
+		then bus_tr = tr break end 
 	end
 
 	if not bus_tr then
@@ -73,7 +74,7 @@ local bus_tr
 	return r.defer(no_undo) end
 
 SEND_MODE = tonumber(SEND_MODE) or 1 -- default to 1 (post-fader)
-SEND_MODE = SEND_MODE < 3 and SEND_MODE-1 or SEND_MODE -- to conform to 0-based index of post-fader (0) and pre-fx (1)
+SEND_MODE = (SEND_MODE > 0 and SEND_MODE < 3) and SEND_MODE-1 or SEND_MODE > 3 and 1 or SEND_MODE -- -1 to conform to 0-based index of post-fader (0) and pre-fx (1), excluding illegal numbers
 
 r.Undo_BeginBlock()
 
