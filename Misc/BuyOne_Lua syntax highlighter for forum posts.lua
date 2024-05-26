d32a1f0cd1b3b01af1f2ed270a80d03ad5a9b4eb
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-05-26 16:38:12 +0300

    Update BuyOne_Lua syntax highlighter for forum posts.lua

--- Misc/BuyOne_Lua syntax highlighter for forum posts.lua
+++ Misc/BuyOne_Lua syntax highlighter for forum posts.lua
@@ -88,7 +88,9 @@ COLOR_LUA_LIBS = "1"
 
 -- All ReaScript API functions, incl. 3d party,
 -- only applies to functions which include 'reaper' or 'gfx'
--- field in their name
+-- field in their name, otherwise these will be colored
+-- accordng to the USER_FUNCTION setting provided
+-- the COLOR_USER_FUNCTIONS setting is enabled
 COLOR_REAPER_LIB = "1"
 
 -- Color separator dot between library and method names,
