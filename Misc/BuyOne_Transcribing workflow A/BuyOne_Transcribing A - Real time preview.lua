cd1a8c7f792f439927ca267d05462a6e70c481b7
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-09-25 18:41:08 +0300

    Update and rename BuyOne_Transcribing - Real time preview.lua to BuyOne_Transcribing A - Real time preview.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Real time preview.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Real time preview.lua
@@ -1,5 +1,5 @@
 --[[
-ReaScript name: BuyOne_Transcribing - Real time preview.lua
+ReaScript name: BuyOne_Transcribing A - Real time preview.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
 Version: 1.5
