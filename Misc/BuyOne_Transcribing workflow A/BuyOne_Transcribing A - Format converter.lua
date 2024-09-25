42c05d824ac775a6859a1599acb21bbb01ba1288
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-09-25 18:47:48 +0300

    Update BuyOne_Transcribing A - Format converter.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Format converter.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Format converter.lua
@@ -1,5 +1,5 @@
 --[[
-ReaScript name: BuyOne_Transcribing - Format converter.lua
+ReaScript name: BuyOne_Transcribing A - Format converter.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
 Version: 1.2
