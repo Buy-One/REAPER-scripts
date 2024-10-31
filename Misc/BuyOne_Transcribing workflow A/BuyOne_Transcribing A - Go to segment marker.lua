f4475be5725232455f67e3e13dd6514ca4a8fc9c
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-10-31 16:55:02 +0200

    Update BuyOne_Transcribing A - Go to segment marker.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Go to segment marker.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Go to segment marker.lua
@@ -16,7 +16,8 @@ About:	The script is part of the Transcribing A workflow set of scripts
 	BuyOne_Transcribing A - Prepare transcript for rendering.lua  
 	BuyOne_Transcribing A - Select Notes track based on marker at edit cursor.lua
 	BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua  
-	BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua
+	BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua  
+	BuyOne_Transcribing A - Search the transcript.lua
 
 	It's a kind of the opposite of the script  
 	'BuyOne_Transcribing A - Select Notes track based on marker at edit cursor.lua'
