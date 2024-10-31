c2c18a34a881a6dbadb57dcc1f72cc768bc8408f
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-10-31 16:56:05 +0200

    Update BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua
@@ -15,7 +15,8 @@ About:	The script is part of the Transcribing A workflow set of scripts
 	BuyOne_Transcribing A - Prepare transcript for rendering.lua   
 	BuyOne_Transcribing A - Select Notes track based on marker at edit cursor.lua  
 	BuyOne_Transcribing A - Go to segment marker.lua
-	BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua
+	BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua  
+	BuyOne_Transcribing A - Search the transcript.lua
 
 	The script can be used for general purposes because it
 	affects all markers in time selection, which is something
