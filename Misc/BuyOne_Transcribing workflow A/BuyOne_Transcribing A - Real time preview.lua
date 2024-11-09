4e7b888f59b956213d0478de3a3a5be3494f60ea
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-11-09 17:31:43 +0200

    Update BuyOne_Transcribing A - Real time preview.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Real time preview.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Real time preview.lua
@@ -35,7 +35,7 @@ About:	The script is part of the Transcribing A workflow set of scripts
 	BuyOne_Transcribing A - Go to segment marker.lua
 	BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua  
 	BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua  
-	BuyOne_Transcribing A - Search the transcript.lua
+	BuyOne_Transcribing A - Search or replace text in the transcript.lua
 	
 	meant to display the transcript segment by segment in real time
 	while REAPER is in play mode or when the edit cursor is located
