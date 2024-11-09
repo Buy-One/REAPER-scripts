89e6531eaa5fcda72ad4989cf775552b084bbe5a
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-11-09 17:30:05 +0200

    Update BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua
@@ -28,6 +28,7 @@ About:	The script is part of the Transcribing A workflow set of scripts
 	BuyOne_Transcribing A - Select Notes track based on marker at edit cursor.lua  
 	BuyOne_Transcribing A - Go to segment marker.lua  
 	BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua
+	BuyOne_Transcribing A - Search or replace text in the transcript.lua
 
 	The script generates a ReaperMenu file for a toolbar 
 	to which all scripts and custom actions included in this 
