3b01515840aa466285582fdfa93e9bc124906bff
Buy-One <61803004+Buy-One@users.noreply.github.com>
2024-11-09 17:29:31 +0200

    Update BuyOne_Transcribing A - Format converter.lua

--- Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Format converter.lua
+++ Misc/BuyOne_Transcribing workflow A/BuyOne_Transcribing A - Format converter.lua
@@ -20,7 +20,7 @@ About:	The script is part of the Transcribing A workflow set of scripts
 	BuyOne_Transcribing A - Go to segment marker.lua
 	BuyOne_Transcribing A - Generate Transcribing A toolbar ReaperMenu file.lua  
 	BuyOne_Transcribing A - Offset position of markers in time selection by specified amount.lua  
-	BuyOne_Transcribing A - Search the transcript.lua
+	BuyOne_Transcribing A - Search or replace text in the transcript.lua
 	
 	meant to format a transcript created with the script 
 	'BuyOne_Transcribing A - Create and manage segments.lua'
