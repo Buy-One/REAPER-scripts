e5613814988950f9924a49b9743b9a5086806492
Buy-One <61803004+Buy-One@users.noreply.github.com>
2025-02-17 21:23:50 +0200

    Update BuyOne_Send selected tracks to designated track for monitoring.lua

--- Tracks/BuyOne_Send selected tracks to designated track for monitoring.lua
+++ Tracks/BuyOne_Send selected tracks to designated track for monitoring.lua
@@ -2,8 +2,11 @@
 ReaScript name: BuyOne_Send selected tracks to designated track for monitoring.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.0
-Changelog: #Initial release
+Version: 1.1
+Changelog: #Added a USER SETTING to include in the routing to the designated track
+	  send destination tracks of the selected tracks along their entire send tree.
+	  #Done away with 'ReaScript task control' dialogue when the script is re-launched
+	  while already running in AUTO mode. Relevant for users of REAPER 7.03+
 Licence: WTFPL
 REAPER: at least v5.962
 Provides: [main=main,midi_editor] .
@@ -60,6 +63,23 @@ About:	The script creates sends from selected tracks
 -- must be exact disregarding leading and trailing spaces
 MONITOR_TRACK_NAME = "Current"
 
+-- Insert any alphanumeric character between the quotes
+-- to enable routing to the designated track track send
+-- destination tracks along the entire send tree which starts
+-- with selected tracks
+-- RULES:
+-- 1. All send destination tracks along the entire send tree
+-- which starts with selected tracks
+-- (hereinafter 'send destination tracks') are routed to the
+-- designated track.
+-- 2. Send destination tracks whose Master/Parent send AKA
+-- Master send (depending on REAPER build) is disabled aren't
+-- routed to the designated track.
+-- 3. Send destination tracks of child tracks in a folder whose
+-- parent track is routed to the designated track are also
+-- routed to the designated track.
+ROUTE_SEND_DESTINATION_TRACKS = ""
+
 -- To enable, insert any alphanumeric
 -- character between the quotes
 AUTO = ""
@@ -75,6 +95,7 @@ function Msg(param, cap) -- caption second or none
 local cap = cap and type(cap) == 'string' and #cap > 0 and cap..' = ' or ''
 reaper.ShowConsoleMsg(cap..tostring(param)..'\n')
 end
+
 
 function no_undo()
 do return end
@@ -144,26 +165,6 @@ return function() func(table.unpack(t)) end
 end
 
 
-function REAPER_Ver_Check(build, want_later, want_earlier, want_current)
--- build is REAPER build number or sring, the function must be followed by 'do return end'
--- want_later, want_earlier and want_current are booleans
--- obviously want_later and want_earlier are mutually exclusive
--- want_later includes current, want_earlier is the up-to version
-local build = build and tonumber(build)
-cur_buld = tonumber(r.GetAppVersion():match('[%d%.]+'))
-local later, earlier, current = cur_buld >= build, cur_buld < build, cur_buld == build
-local err = '   the script requires \n\n  '
-local err = not later and err..'reaper '..build..' and later '
-or not earlier and err..'reaper not later than '..build
-	if err then
-	local x,y = r.GetMousePosition()
-	err = err:upper():gsub('.','%0 ')
-	r.TrackCtl_SetToolTip(err, x, y+10, true) -- topmost true
-	return true
-	end -- 'ReaScript:Run' caption is displayed in the menu bar but no actual undo point is created because Undo_BeginBlock() isn't yet initialized, here and elsewhere
-end
-
-
 function Get_Designated_Track(tr, MONITOR_TRACK_NAME)
 local tr_name = tr and r.GetTrackState(tr)
 	if tr and tr_name:match('[%w%p]*[%w%p]') == MONITOR_TRACK_NAME
@@ -214,6 +215,83 @@ function Remove_Track_Receives(tr)
 end
 
 
+function get_track_parents(parent)
+	return function()
+	parent = r.GetParentTrack(parent) -- assigning to a global value (or upvalue passed as the argument) is crucial to make it work so that the var is constantly updated during the loop, returning r.GetParentTrack(parent) directly won't work because the var isn't updated, the var won't be accessible outside of the loop
+	return parent
+	end
+end
+
+function Is_Track_Parent_Included(tr, t)
+	for parent in get_track_parents(tr) do
+		if t[parent] and
+		r.GetMediaTrackInfo_Value(parent, 'B_MAINSEND') == 1
+		then
+		return true end
+	end
+end
+
+
+function get_track_children_and_grandchildren(tr)
+local st_idx = r.CSurf_TrackToID(tr, false) -- mcpView false // starting loop from the 1st child
+local depth = r.GetTrackDepth(tr)
+return function()
+	local chld_tr = r.GetTrack(0,st_idx)
+		if chld_tr and r.GetTrackDepth(chld_tr) > depth then
+		st_idx=st_idx+1
+		return chld_tr
+		end
+	end
+end
+
+
+function Collect_Tracks(t, fin)
+
+	local function collect_send_dest_tracks(i, t, tr)
+	local snd_cnt = r.GetTrackNumSends(tr, 0) -- category 0 sends
+		if snd_cnt > 0 then
+			for snd_idx=0, snd_cnt-1 do
+			local dest_tr = r.GetTrackSendInfo_Value(tr, 0, snd_idx, 'P_DESTTRACK') -- category 0 send
+				if not t[dest_tr] then -- if it's not one of the selected or stored inside the recursive loop
+				t[#t+1] = dest_tr
+				t[dest_tr] = '' -- dummy value // store to be able to evaluate whether the track has already been stored without having to search for it in the entire table
+				end
+			end
+		t = Collect_Tracks(t,i+1) -- go recursive collecting the entire send tree, fin arg is i+1 to stop where the main loop inside Collect_Tracks() left off for efficiency because the table length may increase and running full table loop is likely to cause endless recursive loop because the same sends will be detected over and over in tracks which have already been processed in higher lever recursive loops
+		end
+	return t
+	end
+
+local fin = fin or 1
+
+	for i=#t,fin,-1 do -- in reverse because of removal of irrelevant tracks
+	local tr = t[i]
+		if r.GetMediaTrackInfo_Value(tr, 'B_MAINSEND') == 0 -- isn't routed to Master
+		or Is_Track_Parent_Included(tr, t) -- a parent of a child track has already been stored and has its Master/parent send enabled so no need to store and route the child track as well
+		then
+		table.remove(t,i)
+		t[tr] = false
+		end
+
+		if ROUTE_SEND_DESTINATION_TRACKS then
+
+		-- Get destinaton tracks of the track and its children (if any) sends
+
+		if r.GetMediaTrackInfo_Value(tr, 'I_FOLDERDEPTH') == 1 and t[tr] then -- current track is a folder or subfolder parent track and hasn't been deleted above so is going to be routed to the designated track while its children won't so their send desitnation tracks must be included in the routing
+			for child in get_track_children_and_grandchildren(tr) do
+			t = collect_send_dest_tracks(i, t, child) -- passing table index of the current track as i argument because the child track is not stored and passing its index would be wrong because it would refer to a completely different track in the table
+			end
+		end
+		t = collect_send_dest_tracks(i, t, tr)
+		end
+
+	end
+
+return t
+
+end
+
+
 function Create_Sends_From_Sel_Tracks(mon_tr)
 	if mon_tr then
 		if r.GetMediaTrackInfo_Value(mon_tr, 'B_MAINSEND') == 1 then
@@ -225,19 +303,50 @@ function Create_Sends_From_Sel_Tracks(mon_tr)
 		-- the designated track is the only one selected
 		Remove_Track_Receives(mon_tr)
 		end
-	local tr_numbers = {} -- to be used in the undo point description
-		for i=0, r.CountTracks(0)-1 do
+	local tr_numbers = {} -- to be used in the undo point description and in Track_Selection_Changed() function
+	local tr_t = {} -- collect tracks to send to the designated track
+
+		for i=0, r.CountTracks(0)-1 do -- counting all tracks instead of selected to be able to get actual indices for storage in the table
 		local tr = r.GetTrack(0,i)
 			if r.IsTrackSelected(tr) then
-			r.CreateTrackSend(tr, mon_tr)
 			tr_numbers[#tr_numbers+1] = i+1
+			-- Collect selected tracks
+				if not tr_t[tr] then -- prevent adding and processing selected tracks which were already added and processed in previous loop cycles as send destination tracks of other tracks in case ROUTE_SEND_DESTINATION_TRACKS is enabled // will also be valid before any track was added and when ROUTE_SEND_DESTINATION_TRACKS is disabled, so covers all cases
+				tr_t[#tr_t+1] = tr
+					if ROUTE_SEND_DESTINATION_TRACKS then -- collect selected tracks send destination tracks along the enire send tree
+					tr_t[tr] = '' -- dummy value
+					tr_t = Collect_Tracks(tr_t, fin)
+					end
+				end
 			end
 		end
+
+	--[[-- OR
+			-- First collect all selected tracks
+			for i=0, r.CountTracks(0)-1 do -- counting all tracks instead of selected to be able to get actual indices for storage in the table
+			local tr = r.GetTrack(0,i)
+				if r.IsTrackSelected(tr) then
+				tr_numbers[#tr_numbers+1] = i+1
+				tr_t[#tr_t+1] = tr
+				tr_t[tr] = '' -- dummy value
+				end
+			end
+
+			-- Collect selected tracks send destination tracks along the enire send tree
+			if ROUTE_SEND_DESTINATION_TRACKS then
+			tr_t = Collect_Tracks(tr_t, fin)
+			end
+	]]
+
+		for k, tr in ipairs(tr_t) do -- create sends from the stored tracks to the esignated track
+		r.CreateTrackSend(tr, mon_tr)
+		end
+
 		-- disable MIDI sends https://forum.cockos.com/showthread.php?t=287218
 		for i=0, r.GetTrackNumSends(mon_tr, -1)-1 do -- category -1 receives
 		r.SetTrackSendInfo_Value(mon_tr, -1, i, 'I_MIDIFLAGS', 31) -- category -1 receives
 		end
-	return tr_numbers
+	return tr_numbers, tr_t
 	end
 end
 
@@ -281,13 +390,20 @@ is_new_value, scr_name, sect_ID, cmd_ID, mode, resol, val = r.get_action_context
 	r.Undo_BeginBlock()
 	end
 
-tr_numbers = Create_Sends_From_Sel_Tracks(mon_tr)
+
+ROUTE_SEND_DESTINATION_TRACKS = #ROUTE_SEND_DESTINATION_TRACKS:gsub(' ','') > 0
+
+local tr_numbers, tr_t = Create_Sends_From_Sel_Tracks(mon_tr)
+local snd_dest_trks = ROUTE_SEND_DESTINATION_TRACKS and #tr_t > #tr_numbers and ' and their send tree' or ''
 
 	if not AUTO then
-	r.Undo_EndBlock('Send tracks '..table.concat(tr_numbers, ', ')..'  to designated monitor track',-1)
-	end
-
-	if AUTO then
+	r.Undo_EndBlock('Send tracks '..table.concat(tr_numbers, ', ')..snd_dest_trks..'  to designated monitor track',-1)
+	else
+		-- prevent ReaScript task control dialogue when the running script is clicked again to be terminated,
+		-- supported since build 7.03
+		-- script flag for auto-relaunching after termination in reaper-kb.ini is 4, e.g. SCR 4, but if changed
+		-- directly while REAPER is running the change doesn't take effect, so in builds older than 7.03 user input is required
+		if r.set_action_options then r.set_action_options(1) end	
 	Re_Set_Toggle_State(sect_ID, cmd_ID, 1)
 	RUN()
 	end
