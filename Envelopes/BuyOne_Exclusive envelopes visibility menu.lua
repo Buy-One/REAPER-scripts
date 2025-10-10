e25920fafc842570802ce17a0c775a7d8b0dce7e
Buy-One <61803004+Buy-One@users.noreply.github.com>
2025-10-10 20:31:17 +0300

    Update BuyOne_Exclusive envelopes visibility menu.lua

--- Envelopes/BuyOne_Exclusive envelopes visibility menu.lua
+++ Envelopes/BuyOne_Exclusive envelopes visibility menu.lua
@@ -9,36 +9,36 @@ REAPER: at least v5.962
 Extensions: 
 Provides: [main=main,midi_editor] .
 About: 	Inspired by actions introduced in build 7.47
-			Track: Hide envelope, display next/previous envelope on same track (cycle)
-			
-			The menu is designed to toggle exclusive visibility 
-			of active envelopes, so that only one envelope is
-			visible at a time.  			
-			The script doesn't allow making multiple envelopes 
-			of the same object visible at once.  
-			If multiple envelopes have been made visible manually, 
-			clicking one visible envelope menu item hides all other 
-			visible envelopes while the envelope of the clicked menu 
-			item remains visible.  
-			In REAPER when an armed envelope is bypassed, it's 
-			automatically unarmed, therefore a checkmark will be 
-			cleared from  
-			'Toggle arming of the visible envelope' menu item when 
-			'Toggle bypass of the visible envelope' menu item is clicked
-			And vice versa, when a bypassed envelope is armed, it's 
-			automatically unbypassed, therefore a checkmark will be 
-			cleared from 
-			'Toggle bypass of the visible envelope' menu item when 
-			'Toggle arming of the visible envelope' menu item is clicked
-			
-			The object targeting priority is as follows: 
-			take under mouse
-			track under mouse
-			parent take of the selected take envelope
-			parent track of the selected track envelope
-			active take of selected item
-			selected track
-			last touched track
+		Track: Hide envelope, display next/previous envelope on same track (cycle)
+		
+		The menu is designed to toggle exclusive visibility 
+		of active envelopes, so that only one envelope is
+		visible at a time.  			
+		The script doesn't allow making multiple envelopes 
+		of the same object visible at once.  
+		If multiple envelopes have been made visible manually, 
+		clicking one visible envelope menu item hides all other 
+		visible envelopes while the envelope of the clicked menu 
+		item remains visible.  
+		In REAPER when an armed envelope is bypassed, it's 
+		automatically unarmed, therefore a checkmark will be 
+		cleared from  
+		'Toggle arming of the visible envelope' menu item when 
+		'Toggle bypass of the visible envelope' menu item is clicked
+		And vice versa, when a bypassed envelope is armed, it's 
+		automatically unbypassed, therefore a checkmark will be 
+		cleared from 
+		'Toggle bypass of the visible envelope' menu item when 
+		'Toggle arming of the visible envelope' menu item is clicked
+		
+		The object targeting priority is as follows: 
+		take under mouse
+		track under mouse
+		parent take of the selected take envelope
+		parent track of the selected track envelope
+		active take of selected item
+		selected track
+		last touched track
 ]]
 
 
@@ -345,14 +345,14 @@ local is_new_value, scr_path, sect_ID, cmd_ID, mode, resol, val, contextstr = r.
 local x, y = r.GetMousePosition()
 local item, take = r.GetItemFromPoint(x, y, false) -- allow_locked false
 local tr, info = r.GetTrackFromPoint(x, y)
-	if not take and not tr then
+	if not take and not tr then -- no object under mouse
 	tr, take = Is_Selected_Track_Or_Take_Env()
 	end
-	if not take and not tr then
+	if not take and not tr then -- no selected envelope
 	local item = r.GetSelectedMediaItem(0,0)
 	take = item and r.GetActiveTake(item)
-	tr = not take and r.GetSelectedTrack(0,0)
-	tr = tr or r.GetLastTouchedTrack()
+	tr = not take and r.GetSelectedTrack(0,0) -- no selected item
+	tr = tr or r.GetLastTouchedTrack() -- no selected track
 	end
 
 	if not take and not tr then
