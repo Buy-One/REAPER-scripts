b5d31a15c155f1e53a6709d62294ecbb770b1dd5
Buy-One <61803004+Buy-One@users.noreply.github.com>
2025-11-29 22:18:34 +0200

    Fix formatting and indentation in Lua script

--- Misc/BuyOne_Toggle Action list window visibility.lua
+++ Misc/BuyOne_Toggle Action list window visibility.lua
@@ -9,27 +9,27 @@ REAPER: at least v5.962
 Extensions: SWS/S&M or js_ReaScriptAPI
 Provides: [main=main,midi_editor] .
 About:	The script provides the ability to toggle
-			the Action list window, which is not possible
-			with the action 'Show action list' because
-			it doesn't allow closing the window. A floating
-			Action list window can be closed with Esc key
-			but a docked window can't. This script covers
-			both cases.
+		the Action list window, which is not possible
+		with the action 'Show action list' because
+		it doesn't allow closing the window. A floating
+		Action list window can be closed with Esc key
+		but a docked window can't. This script covers
+		both cases.
 
-			If the window is open and docked in a closed docker
-			the script will open the docker along with all
-			other open docked windows.  
-			If the window is closed and docked in a closed split
-			docker the script will open the window along with other 
-			open windows docked on the same side of the split.
+		If the window is open and docked in a closed docker
+		the script will open the docker along with all
+		other open docked windows.  
+		If the window is closed and docked in a closed split
+		docker the script will open the window along with other 
+		open windows docked on the same side of the split.
 
-			In order to be able to toggle the Action list
-			window regardless of the mouse cursor focus
-			bind this script to a shortcut having selected
-			Global+text fields option in the Scope drop-down
-			menu of keyboard shorcut assignment dialogue.
-			Otherwise you will have to bring REAPER main window
-			into focus in order for the shortcut to be registered.
+		In order to be able to toggle the Action list
+		window regardless of the mouse cursor focus
+		bind this script to a shortcut having selected
+		Global+text fields option in the Scope drop-down
+		menu of keyboard shorcut assignment dialogue.
+		Otherwise you will have to bring REAPER main window
+		into focus in order for the shortcut to be registered.
 
 ]]
 
