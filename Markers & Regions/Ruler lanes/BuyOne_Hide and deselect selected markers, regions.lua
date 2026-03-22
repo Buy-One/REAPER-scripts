bd8df26d762dc66e96ef26ccc6e27674d01e6eb9
Buy-One <61803004+Buy-One@users.noreply.github.com>
2026-03-22 16:18:36 +0200

    Create BuyOne_Hide and deselect selected markers, regions.lua

---
+++ Markers & Regions/Ruler lanes/BuyOne_Hide and deselect selected markers, regions.lua
@@ -0,0 +1,159 @@
+--[[
+ReaScript name: BuyOne_Hide and deselect selected markers, regions.lua
+Author: BuyOne
+Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
+Version: 1.0
+Changelog: #Initial release
+Licence: WTFPL
+REAPER: at least v7.62
+Provides: [main=main,midi_editor] .
+About: See the script name.
+
+]]
+
+
+local Debug = ""
+function Msg(param, cap) -- caption second or none
+	if #Debug:gsub(' ','') > 0 then -- OR Debug:match('%S') // declared outside of the function, allows to only didplay output when true without the need to comment the function out when not needed, borrowed from spk77
+	local cap = cap and tostring(cap)..' = ' or ''
+	reaper.ShowConsoleMsg(cap..tostring(param)..'\n')
+	end
+end
+
+local r = reaper
+
+
+function no_undo()
+do return end
+end
+
+
+
+function Error_Tooltip(text, caps, spaced, x2, y2, want_color, want_blink)
+-- the tooltip sticks under the mouse within Arrange
+-- but quickly disappears over the TCP, to make it stick
+-- just a tad longer there it must be directly under the mouse
+-- not directly under the mouse the tooltip sticks if mouse is over Arrange
+-- but soon disappears if mouse is in the TCP area but not over the TCP
+-- and immediately disappears if the mouse is over the TCP
+-- caps and spaced are booleans, caps doesn't apply to non-ANSI characters
+-- x2, y2 are integers to adjust tooltip position by
+-- want_color is boolean to enable temporary ruler coloring to emphasize the error
+-- want_blink is boolean to enable ruler color blinking
+local x, y = r.GetMousePosition()
+--[[ IF USING WITH gfx
+local x, y = 0,0 -- set to 0 so that they can be overridden with x2 and y2 arguments which are passed as gfx.clienttoscreen(0,0) so that the tooltip is displayed over the gfx window
+]]
+local text = caps and text:upper() or text
+local utf8 = '[\0-\127\194-\244][\128-\191]*'
+local text = spaced and text:gsub(utf8,'%0 ') or text -- supporting UTF-8 char
+local x2, y2 = x2 and math.floor(x2) or 0, y2 and math.floor(y2) or 0
+r.TrackCtl_SetToolTip(text, x+x2, y+y2, true) -- topmost true
+-- r.TrackCtl_SetToolTip(text:upper(), x, y, true) -- topmost true
+-- r.TrackCtl_SetToolTip(text:upper():gsub('.','%0 '), x, y, true) -- spaced out // topmost true
+	if want_color then
+	local color_init = r.GetThemeColor('col_tl_bg', 0)
+	local color = color_init ~= 255 and 255 or 65535 -- use red or yellow of red is taken
+		if want_blink then
+		    for i = 1, 100 do
+				if i == 1 or i == 40 or i == 80 then
+				r.SetThemeColor('col_tl_bg', color, 0)
+				elseif i == 20 or i == 60 or i == 100 then
+				r.SetThemeColor('col_tl_bg', color_init, 0)
+				end
+			r.UpdateTimeline()
+			end
+		else
+		r.SetThemeColor('col_tl_bg', color, 0) -- Timeline background
+			for i = 1, 200 do -- ensures that the warning color sticks for some time
+			-- without the function inside the loop the end (200) value must be much greater
+			r.UpdateTimeline()
+			end
+		r.SetThemeColor('col_tl_bg', color_init, 0) -- Timeline background // restore the orig color
+		r.UpdateTimeline() -- without this function the color will only be restored when user clicks within the Arrange
+		end
+	end
+--[[
+-- a time loop can be added to run until certain condition obtains, e.g.
+local time_init = r.time_precise()
+repeat
+until condition and r.time_precise()-time_init >= 0.7 or not condition
+]]
+r.UpdateTimeline() -- might be needed because tooltip can sometimes affect graphics
+end
+
+
+
+function REAPER_Ver_Check(build, want_later, want_earlier, want_current)
+-- build is REAPER build number or sring, the function must be followed by 'do return end'
+-- want_later, want_earlier and want_current are booleans
+-- obviously want_later and want_earlier are mutually exclusive
+-- want_later includes current, want_earlier is the up-to version
+local build = build and tonumber(build)
+local cur_buld = tonumber(r.GetAppVersion():match('[%d%.]+'))
+local later, earlier, current = cur_buld >= build, cur_buld < build, cur_buld == build
+local err = '   the script requires \n\n  '
+local err = want_later and not later and err..'reaper '..build..' and later '
+or want_earlier and not earlier and err..'reaper no later than '..build
+or want_current and not current and 'reaper build '..build
+	if err then
+--[[
+	local x,y = r.GetMousePosition()
+	err = err:upper():gsub('.','%0 ')
+	r.TrackCtl_SetToolTip(err, x, y+10, true) -- topmost true
+--]]
+	Error_Tooltip('\n\n '..err..' \n\n', 1, 1) -- caps, spaced true
+	return true
+	end -- 'ReaScript:Run' caption is displayed in the menu bar but no actual undo point is created because Undo_BeginBlock() isn't yet initialized, here and elsewhere
+end
+
+
+
+function Collect_Visible_Selected_Mrkrs_Regns()
+
+local Get = r.GetRegionOrMarkerInfo_Value
+
+	if not Get then return end -- only supported since build 7.62
+
+local GetSet = r.GetSetProjectInfo
+local t = {}
+
+	for i=0, r.GetNumRegionsOrMarkers(0)-1 do
+	local obj = r.GetRegionOrMarker(0, i, '') -- guidStr is empty string, i.e. getting by index
+	local lane_idx = Get(0, obj, 'I_LANENUMBER')
+	local lane_locked = GetSet(0, 'RULER_LANE_LOCKED:'..lane_idx, 0, false) == 1
+	local lane_vis = GetSet(0, 'RULER_LANE_HIDDEN:'..lane_idx, 0, false) == 0 -- isSet false // the attribute works without the colon as well // essentially redundant because B_VISIBLE is also false when the entire lane is hidden
+		if lane_vis and not lane_locked and Get(0, obj, 'B_VISIBLE') == 1 and Get(0, obj, 'B_UISEL') == 1 then
+		table.insert(t, obj)
+		end
+	end
+
+return t
+
+end
+
+
+
+Error_Tooltip('') -- clear other tooltips, such as toolbar button tooltip if the script is executed from a toolbar button
+
+	if REAPER_Ver_Check(7.62, 1) -- want_later true
+	then return r.defer(no_undo) end
+
+local is_new_value, scr_name, sect_ID, cmd_ID, mode, resol, val, contextstr = r.get_action_context()
+local scr_name = scr_name:match('.+_(.+)%.%w+') -- without path, scripter name & ext
+
+local t = Collect_Visible_Selected_Mrkrs_Regns()
+
+	if #t == 0 then
+	Error_Tooltip('\n\n\tno visible selected \n\n\t markers/regions \n\n or their lanes are locked \n\n', 1, 1) -- caps, spaced true
+	return r.defer(no_undo) end
+
+r.Undo_BeginBlock()
+
+	for idx, obj in ipairs(t) do
+	r.SetRegionOrMarkerInfo_Value(0, obj, 'B_HIDDEN', 1)
+	r.SetRegionOrMarkerInfo_Value(0, obj, 'B_UISEL', 0)
+	end
+
+r.Undo_EndBlock(scr_name,-1)
+
