71814bb32ac3ade06b2a28afbc8e2cda9a0da369
Buy-One <61803004+Buy-One@users.noreply.github.com>
2026-04-20 16:33:13 +0300

    Update BuyOne_Select tracks with Razor edit areas (emulation of PT Link track and edit selection).lua

--- Tracks/BuyOne_Select tracks with Razor edit areas (emulation of PT Link track and edit selection).lua
+++ Tracks/BuyOne_Select tracks with Razor edit areas (emulation of PT Link track and edit selection).lua
@@ -2,31 +2,39 @@
 ReaScript name: BuyOne_Select tracks with Razor edit areas (emulation of PT Link track and edit selection).lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.0
-Changelog: #Initial release
+Version: 1.1
+Changelog: #Improved track selection consistency when left mouse button 
+			is clicked while the mouse cursor is over Arrange
 Licence: WTFPL
 REAPER: at least v5.962
-Extensions: 
+Extensions: JS_ReaScriptAPI recommended for better performance
 Provides: [main] .
 About: 	The script is an emulation of a Pro Tools feature
-		'Link track to edit selection'.  
-		It selects track as soon as razor edit area is created 
-		on it and deslects it as soon as one is cleared.
+			'Link track to edit selection'.  
+			It selects track as soon as razor edit area is created 
+			on it and deslects it as soon as one is cleared.
 
-		Once launched the script runs in the background. If linked
-		to a toolbar button or a menu item these will be lit or
-		checkmarked respectively. To terminate the script launch it
-		again. This offers toggle behavior similar to the said 
-		Pro Tools feature.
+			Once launched the script runs in the background. If linked
+			to a toolbar button or a menu item these will be lit or
+			checkmarked respectively. To terminate the script launch it
+			again. This offers toggle behavior similar to the said 
+			Pro Tools feature.
 
-		One quirk of the script stemming from the ReaScript API 
-		is that tracks with razor edit areas on them get temporarily 
-		deselected for as long as left mouse button is depressed 
-		while the mouse cursor hovers over a razor edit area. Once 
-		released the selection is reinstated.
+			One quirk of the script stemming from the ReaScript API 
+			is that tracks with razor edit areas on them get temporarily 
+			deselected for as long as left mouse button is depressed 
+			while the mouse cursor hovers over the Arrange. Once 
+			released the selection is reinstated.  
+			Having JS_ReaScriptAPI installed helps to fix this behavior.
+			When installed, tracks do not get deselected in response
+			to left mouse click over Arrange. The tradeoff is that when 
+			razor edit area position or vertical boundaries are changed 
+			with left mouse drag, tracks on which Razor edit area is 
+			cleared are only deselected once left mouse button is released.
 
-		See USER SETTINGS
+			See USER SETTINGS
 ]]
+
 
 
 -----------------------------------------------------------------------------
@@ -82,24 +90,29 @@ function SELECT()
 -- while the mouse cursor is over a razor edit area
 -- because in this scenario the function GetSetMediaTrackInfo_String() returns empty razor edit data
 -- which the script relies on, so having not detected such data it deselects tracks
+-- https://forum.cockos.com/showthread.php?t=308465
+-- fixed by preventing track selection update for as long as left mouse button is depressed
+-- and mouse cursor is located within Arrange
 	for i=0, r.GetNumTracks()-1 do
 	local tr = r.GetTrack(0, i)
 	local ret, exist = r.GetSetMediaTrackInfo_String(tr, 'P_RAZOREDITS','',false) -- setNewValue false
 	local exist = #exist > 0
-	local ret, RE_track = r.GetSetMediaTrackInfo_String(tr, 'P_EXT:LINK_TRACK_AND_EDIT_SELECTION', '', false) -- setNewValue false
-	RE_track = #RE_track > 0
+	local ret, RE = r.GetSetMediaTrackInfo_String(tr, 'P_EXT:LINK_TRACK_AND_EDIT_SELECTION', '', false) -- setNewValue false
+	local RE_track = #RE > 0
 	local is_sel = r.IsTrackSelected(tr)
-		if EXCLUSIVE and exist ~= is_sel then -- don't run when the selection state already matches the status
-		r.SetTrackSelected(tr, exist) -- toggle, select track with razor edit areas and deselect without
-		elseif RE_track or exist then -- either former or current razor edit area track; if former, it will be deselected and the extended data will be cleared
-		local status = RE_track and not exist and '' or exist and '1' -- if former razor edit area track without active razor edits, the extended data will be cleared
-			if status == '' or not RE_track then -- update track extended data depending on the presence of razor edit areas
-			r.GetSetMediaTrackInfo_String(tr, 'P_EXT:LINK_TRACK_AND_EDIT_SELECTION', status, true) -- setNewValue true
-			end
-		-- de/select tracks with razor edit areas, leaving other tracks selected
-		status = status == '1'
-			if status ~= is_sel then -- don't run when the selection state already matches the status
-			r.SetTrackSelected(tr, status)
+		if not r.JS_Mouse_GetState or r.JS_Mouse_GetState(1) ~= 1 then -- only change selection state when there's no left mouse click as a means to overcome the problem described at the start of the function
+			if EXCLUSIVE and exist ~= is_sel then -- don't run when the selection state already matches the status
+			r.SetTrackSelected(tr, exist) -- toggle, select track with razor edit areas and deselect without
+			elseif RE_track or exist then -- either former or current razor edit area track; if former, it will be deselected and the extended data will be cleared
+			local status = RE_track and not exist and '' or exist and '1' -- if former razor edit area track without active razor edits, the extended data will be cleared
+				if status == '' or not RE_track then -- update track extended data depending on the presence of razor edit areas
+				r.GetSetMediaTrackInfo_String(tr, 'P_EXT:LINK_TRACK_AND_EDIT_SELECTION', status, true) -- setNewValue true
+				end
+			-- de/select tracks with razor edit areas, leaving other tracks selected
+			status = status == '1'
+				if status ~= is_sel then -- don't run when the selection state already matches the status
+				r.SetTrackSelected(tr, status)
+				end
 			end
 		end
 	end
