ece0d79dff26bb2309515d5d493e5614bf7ec72b
Buy-One <61803004+Buy-One@users.noreply.github.com>
2026-05-28 20:34:11 +0300

    Update BuyOne_Duplicate project marker_META.lua

--- Markers & Regions/BuyOne_Duplicate project marker_META.lua
+++ Markers & Regions/BuyOne_Duplicate project marker_META.lua
@@ -2,8 +2,8 @@
 ReaScript name: BuyOne_Duplicate project marker_META.lua (9 scripts)
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.0
-Changelog: #Initial release
+Version: 1.1
+Changelog: #Made directory validation cross-platform in the function which spawns individual scripts
 Licence: WTFPL
 REAPER: at least v5.962
 Extensions: 
@@ -56,12 +56,18 @@ end
 
 function META_Spawn_Scripts(fullpath, fullpath_init, scr_name, names_t)
 
-	local function Dir_Exists(path) -- short
-	local path = path:match('^%s*(.-)%s*$') -- remove leading/trailing spaces
+	local function Dir_Exists(path)
+	local path = path:match('^%s*(.-)%s*$') -- remove leading/trailing spaces // OR ('(%S.+)%s*$')
 	local sep = path:match('[\\/]')
-	local path = path:match('.+[\\/]$') and path:sub(1,-2) or path -- last separator is removed to return 1 (valid)
-	local _, mess = io.open(path)
-	return mess:match('Permission denied') and path..sep -- dir exists // this one is enough
+		if not sep then
+			-- if path is disk root where the separator isn't listed, use forward slash, which should work on Windows as well
+			if path:match('^%u:$') then sep = '/'
+			else return -- likely not a string representing a path
+			end
+		end
+	path = path:match('.+[\\/]$') and path:sub(1,-2) or path -- last separator is removed so the path is properly formatted for os.rename()
+	local ok, mess, code = os.rename(path, path)
+	return (ok or code == 13) and path..sep -- 13 is error code for 'exists but permission denied' on some systems
 	end
 
 	local function Esc(str)
