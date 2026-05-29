e7736d171d6236dadb18250118a975552297fb7c
Buy-One <61803004+Buy-One@users.noreply.github.com>
2026-05-29 12:41:03 +0300

    Update BuyOne_Lua syntax highlighter for forum posts.lua

--- Misc/BuyOne_Lua syntax highlighter for forum posts.lua
+++ Misc/BuyOne_Lua syntax highlighter for forum posts.lua
@@ -2,15 +2,16 @@
 ReaScript name: BuyOne_Lua syntax highlighter for forum posts.lua
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058 or https://github.com/Buy-One/REAPER-scripts/issues
-Version: 1.4
-Changelog: 1.4 	#Added support for retrieving original code from the clipboard
+Version: 1.5
+Changelog:  1.5 #Made directory validation method cross-platform
+			1.4 #Added support for retrieving original code from the clipboard
 				#Updated 'About' text
-		   1.3 	#Optimized preliminary evaluation of the input string
+			1.3 #Optimized preliminary evaluation of the input string
 				#Fixed formatting of nested REAPER and user fuctions
 				#Fixed some corner cases in user function formatting
-		   1.2	#Fixed last line capture if the INPUT setting closure is located 
+			1.2 #Fixed last line capture if the INPUT setting closure is located 
 				on the last line of the string rather than on the next line
-		   1.1 	#Fixed last line capture if the script is run from inside REAPER
+			1.1 #Fixed last line capture if the script is run from inside REAPER
 				#Made sure that the item with the output is placed on the same track 
 				as the item with the input when the script is run from inside REAPER
 Licence: WTFPL
@@ -1236,13 +1237,25 @@ end
 
 
 
-function Dir_Exists(path) -- short
-local path = path:match('^%s*(.-)%s*$') -- remove leading/trailing spaces
+function Dir_Exists(path)
+local path = path:match('^%s*(.-)%s*$') -- remove leading/trailing spaces // OR ('(%S.+)%s*$')
 local sep = path:match('[\\/]')
-	if not sep then return end -- likely not a string represening a path
-local path = path:match('.+[\\/]$') and path:sub(1,-2) or path -- last separator is removed to return 1 (valid)
-local _, mess = io.open(path)
-return mess:match('Permission denied') and path..sep -- dir exists // this one is enough
+	if not sep then
+		-- if path is disk root where the separator isn't listed, use forward slash, which should work on Windows as well
+		if path:match('^%u:$') then sep = '/'
+		else return -- likely not a string representing a path
+		end
+	end
+path = path:match('.+[\\/]$') and path:sub(1,-2) or path -- last separator is removed so the path is properly formatted for os.rename()
+local OS = r.GetAppVersion()
+local win = OS:match('/') or OS:match('/x')
+	if win then
+	local _, mess = io.open(path)
+	return #path:gsub('[%c%.]', '') > 0 and mess and mess:match('Permission denied') and path..sep -- dir exists // this one is enough HOWEVER THIS IS ALSO THE RESULT IF THE path var ONLY INCLUDES DOTS, therefore gsub ensures that besides dots there're other characters
+	else
+	local ok, mess, code = os.rename(path, path)
+	return (ok or code == 13) and path..sep -- 13 is error code for 'exists but permission denied' on some systems
+	end
 end
 
 
